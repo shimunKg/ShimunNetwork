f1f921ee1f520a4faeb0199471e1f54d8771826c
Nikola Jovanovic Simunic <shimun_kg@icloud.com>
2023-12-11 19:01:30 +0100

    request fetch made public

--- Sources/ShimunNetwork/HTTPSRequestFetch.swift
+++ Sources/ShimunNetwork/HTTPSRequestFetch.swift
@@ -11,4 +11,9 @@ import Foundation
 public struct HTTPSRequestFetch<R: Decodable> {
     let result: R
     let response: HTTPURLResponse
+    
+    public init(result: R, response: HTTPURLResponse) {
+        self.result = result
+        self.response = response
+    }
 }
